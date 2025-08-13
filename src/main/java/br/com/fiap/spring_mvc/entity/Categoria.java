95b3fb21a05d7f25a9125f6c1ba86acf8d1632e9
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-08-12 21:42:23 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/spring_mvc/entity/Categoria.java
@@ -0,0 +1,22 @@
+package br.com.fiap.spring_mvc.entity;
+
+public enum Categoria {
+    TERROR("Terror"),
+    ROMANCE("Romance"),
+    COMEDIA("Cómedia"),
+    FICCAO("FIcção"),
+    SUSPENSE("Suspense"),
+    FANTASIA("Fantasia"),
+    BIOGRAFIA("Biografia"),
+    TECNOLOGIA("Tecologia"),
+    ;
+
+    private String descricao;
+
+    Categoria(String descricao) {
+        this.descricao = descricao;
+    }
+        public String getDescricao(){
+
+    }
+}
